bf6ad2614acc21d65aec954249476be01b635e0f
mohhawamdeh <157696341+mohhawamdeh@users.noreply.github.com>
2024-05-14 19:33:07 +0300

    convert to pymysql

--- bsc/bsc/settings.py
+++ bsc/bsc/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import pymysql
+pymysql.version_info = (1, 4, 6, 'final', 0)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
